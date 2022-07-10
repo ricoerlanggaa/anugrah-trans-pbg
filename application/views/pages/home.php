8de25c509ec26031df06ac9ae0d3650dcb075c3c
ricoerlanggaa <ricoerlanggaa@gmail.com>
2022-07-10 17:50:59 +0700

    Add Header Home

---
+++ application/views/pages/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anugrah Trans Purbalingga</title>
+	<link href="<?= base_url('assets/stylesheets/bootstrap.min.css'); ?>" rel="stylesheet">
+</head>
+<body>
+    <?php $this->load->view('partials/HeaderHome'); ?>
+	<script src="<?= base_url('assets/javascript/jquery.min.js'); ?>"></script>
+	<script src="<?= base_url('assets/javascript/bootstrap.min.js'); ?>"></script>
+</body>
+</html>
---
+++ application/views/partials/HeaderHome.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark fixed-top">
+    <div class="container">
+        <a href="#" class="navbar-brand">ANUGRAH TRANS PURBALINGGA</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuItems" aria-controls="menuItems" aria-expanded="false" aria-label="Toggle Navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="menuItems">
+            <div class="me-auto mb-2 mb-lg-0"></div>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">BERANDA</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">CARI TRAVEL</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">TENTANG KAMI</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
